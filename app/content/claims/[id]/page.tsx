c77764f084afa6322f0df9bca91110a3f4acf21c
RezyJS <onlyrezystor@gmail.com>
2025-01-29 17:05:26 +0300

    removed redundant import to build on vercel

--- app/content/claims/[id]/page.tsx
+++ app/content/claims/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image'; // Import the Image component
 
 // Define the Claim interface
 interface Claim {

4562a7deee5aa8ac755fc449ba13165cf1e0aa55
RezyJS <onlyrezystor@gmail.com>
2025-02-01 22:19:26 +0300

    removed Ukraine flag and attribution

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -85,6 +85,7 @@ const MapComponent = () => {
         center={[56.8587, 35.9176]}
         zoom={13}
         style={{ height: '100%', width: '100%' }}
+        attributionControl={false}
       >
         <MapEvents />
         <TileLayer

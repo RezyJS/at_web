6970b5506c1a7671a2f18b30456e3f70b9948ebe
RezyJS <onlyrezystor@gmail.com>
2025-02-06 15:36:55 +0300

    attempt to fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,25 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get('accessToken')?.value;
   const refreshToken = request.cookies.get('refreshToken')?.value;
 
   if (refreshToken && (
     !(
       request.nextUrl.pathname.startsWith('/content') || 
-      request.nextUrl.pathname.startsWith('/api')) || 
+      request.nextUrl.pathname.startsWith('/api')
+    ) || 
       request.nextUrl.pathname === '/content')
     ) {
     return NextResponse.redirect(new URL('/content/news', request.url));
   }
 
   // If no tokens are found and the user is not on an auth page, redirect to login
-  if (!accessToken && !refreshToken && !(
-    request.nextUrl.pathname.startsWith('/auth') || 
-    request.nextUrl.pathname.startsWith('/confirm-login') ||
-    request.nextUrl.pathname.startsWith('/registration')
-  )) {
+  if (!refreshToken && !(
+      request.nextUrl.pathname.startsWith('/auth') || 
+      request.nextUrl.pathname.startsWith('/confirm-login') ||
+      request.nextUrl.pathname.startsWith('/registration')
+    )
+  ) {
     return NextResponse.redirect(new URL('/auth', request.url));
   }
 }

8fdf7da30dae68a8f433ce4d0d02517065c4987c
RezyJS <onlyrezystor@gmail.com>
2025-02-06 15:50:21 +0300

    claims cards size change

--- components/ClaimCard.tsx
+++ components/ClaimCard.tsx
@@ -23,7 +23,7 @@ const ClaimCard: React.FC<ClaimCardProps> = ({ claim }) => {
   return (
     <div
       onClick={handleCardClick}
-      className="border-neutral-200 border-[2px] p-[20px] gap-[10px] rounded-xl max-w-lg w-[75vw] min-w-[320px] flex flex-col justify-between transition-transform duration-300 ease-in-out bg-white transform hover:scale-105 hover:shadow-lg cursor-pointer">
+      className="border-neutral-200 border-[2px] p-[20px] gap-[10px] rounded-xl max-w-sm w-[75vw] min-w-[320px] flex flex-col justify-between transition-transform duration-300 ease-in-out bg-white transform hover:scale-105 hover:shadow-lg cursor-pointer">
         <p className='font-bold text-xl border-b-2 border-b-neutral-200 pb-5 line-clamp-2'>{claim.title}</p>
         <p className='font-medium text-md text-pretty line-clamp-3'>{claim.description}</p>
         <div className='flex justify-between w-full border-t-2 border-t-neutral-200 gap-[20px] pt-5'>
